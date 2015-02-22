6d5b8ecdac7664609fe353cc5a6c3d3df15a568b
patilprajaktab <patilprajaktab@gmail.com>
2015-02-22 01:16:55 -0800

    Recyclier view

---
+++ app/src/main/java/com/example/kuldeep/project/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.kuldeep.project;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private RecipeData[] itemsData;
+
+
+    public MyAdapter(RecipeData[] itemsData) {
+        this.itemsData = itemsData;
+    }
+
+
+    @Override
+
+
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_recipe, null);
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+
+    @Override
+
+
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.txtViewTitle.setText(itemsData[position].getTitle());
+        viewHolder.imgViewIcon.setImageResource(itemsData[position].getImageUrl());
+    }
+
+    @Override
+
+
+    public int getItemCount() {
+        return itemsData.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtViewTitle;
+
+        public ImageView imgViewIcon;
+
+
+        public ViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            txtViewTitle = (TextView) itemLayoutView.findViewById(R.id.item_title);
+            imgViewIcon = (ImageView) itemLayoutView.findViewById(R.id.item_icon);
+        }
+
+    }
+}
